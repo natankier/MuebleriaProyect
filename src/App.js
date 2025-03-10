bddc1bd8c4fc940ab2a515da293945e8b5c297cd
¨Natan <natankier@gmail.com>
2025-03-10 18:55:12 -0300

    Arregle el NavBar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from "./components/NavBar/NavBar"
 import Register from './pages/Register'; // Ruta corregida
 import Home from './pages/Home'; // Ruta corregida
 import OrdersPanel from './pages/OrdersPanel'; // Ruta corregida
 import Login from './pages/Login'; // Ruta corregida
 import Designer3D from './pages/Designer3D'; // Ruta corregida
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
     return (
         <Router>
+                <NavBar />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/register" element={<Register />} />
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,96 +1,35 @@
-// import logo from "../../assets/logoCarne.png";
- 
-// import { Link } from "react-router-dom";
-// import "./NavBar.scss";
-
-// const NavBar = () => {
-//   return (
-//     <nav className="navbar navbar-expand-lg bg-body-tertiary">
-//       <div className="container-fluid">
-//         {/* Logo en la izquierda */}
-//         <Link to="/" className="navbar-brand">
-//           <img src={logo} alt="Logo de la carnicería" className="navbar-logo" />
-//         </Link>
-
-//         {/* Botón de menú desplegable para pantallas pequeñas */}
-//         <button
-//           className="navbar-toggler"
-//           type="button"
-//           data-bs-toggle="collapse"
-//           data-bs-target="#navbarNav"
-//           aria-controls="navbarNav"
-//           aria-expanded="false"
-//           aria-label="Toggle navigation"
-//         >
-//           <span className="navbar-toggler-icon"></span>
-//         </button>
-
-//         {/* Contenido del menú */}
-//         <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
-//           <ul className="navbar-nav mx-auto">
-//             <li className="nav-item">
-//               <Link to="/pages/Home" className="nav-link">
-//                 Home
-//               </Link>
-//             </li>
-//             <li className="nav-item">
-//               <Link to="/pages/Login" className="nav-link">
-//                 Login
-//               </Link>
-//             </li>
-//             <li className="nav-item">
-//               <Link to="/pages/Register" className="nav-link">
-//                 Register
-//               </Link>
-//             </li>
-//             <li className="nav-item">
-//               <Link to="/pages/OrdersPanel" className="nav-link">
-//                 OrdersPanel
-//               </Link>
-//             </li>
-//             <li className="nav-item">
-//               <Link to="/pages/Designer3D" className="nav-link">
-//                 Designer3D
-//               </Link>
-//             </li>
-//           </ul>
-
-
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default NavBar;
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.scss';
 
 const NavBar = () => {
   return (
-    <nav className="navbar">
-      <div className="navbar-brand">
-        <Link to="/" className="navbar-logo">Mi Proyecto</Link>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand">Mi Proyecto</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">Inicio</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/register" className="nav-link">Registro</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/login" className="nav-link">Iniciar Sesión</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/orders" className="nav-link">Pedidos</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/designer" className="nav-link">Diseñador 3D</Link>
+            </li>
+          </ul>
+        </div>
       </div>
-      <ul className="navbar-links">
-        <li>
-          <Link to="/" className="nav-link">Inicio</Link>
-        </li>
-        <li>
-          <Link to="/register" className="nav-link">Registro</Link>
-        </li>
-        <li>
-          <Link to="/login" className="nav-link">Iniciar Sesión</Link>
-        </li>
-        <li>
-          <Link to="/orders" className="nav-link">Pedidos</Link>
-        </li>
-        <li>
-          <Link to="/designer" className="nav-link">Diseñador 3D</Link>
-        </li>
-      </ul>
     </nav>
   );
 };
